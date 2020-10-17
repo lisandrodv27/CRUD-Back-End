5c12940904c757ee027c8c3df0ab042ba9707615
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-10-17 23:30:46 +0100

    Update ProfileService.java

--- src/main/java/com/qa/service/ProfileService.java
+++ src/main/java/com/qa/service/ProfileService.java
@@ -3,7 +3,55 @@ package com.qa.service;
 import java.util.List;
 import java.util.Optional;
 
+importpackage com.qa.service;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.exceptions.ProfileNotFoundException;
+import com.qa.rest.Profile;
+import com.qa.rest.ProfileRepository;
+
+@Service
+public class ProfileService {
+
+	@Autowired
+	private ProfileRepository repository;
+
+	private ProfileService(ProfileRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	public List<Profile> getProfile() {
+		return this.repository.findAll();
+	}
+
+	public Profile createProfile(Profile profile) {
+		return this.repository.save(profile);
+	}
+
+	public boolean deleteProfile(Long id) {
+		this.repository.deleteById(id);
+		return !this.repository.existsById(id);
+	}
+
+	public Profile updateProfile(Profile profile, Long id) {
+		Optional<Profile> optProfile = this.repository.findById(id);
+		Profile oldProfile = optProfile.orElseThrow(() -> new ProfileNotFoundException());
+
+		oldProfile.setName(profile.getName());
+		oldProfile.setNationality(profile.getNationality());
+		oldProfile.setDescription(profile.getDescription());
+
+		Profile updatedProfile = this.repository.save(oldProfile);
+		return updatedProfile;
+
+	}
+} org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.exceptions.ProfileNotFoundException;
