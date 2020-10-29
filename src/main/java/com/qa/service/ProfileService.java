06430bb70e154f5caf7aabcd9a28552497f784f1
lisandrodv27 <71394551+lisandrodv27@users.noreply.github.com>
2020-10-29 12:35:49 +0000

    Update ProfileService.java

--- src/main/java/com/qa/service/ProfileService.java
+++ src/main/java/com/qa/service/ProfileService.java
@@ -51,47 +51,4 @@ public class ProfileService {
 		return updatedProfile;
 
 	}
-} org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.qa.exceptions.ProfileNotFoundException;
-import com.qa.rest.Profile;
-import com.qa.rest.ProfileRepository;
-
-@Service
-public class ProfileService {
-
-	@Autowired
-	private ProfileRepository repository;
-
-	private ProfileService(ProfileRepository repository) {
-		super();
-		this.repository = repository;
-	}
-
-	public List<Profile> getProfile() {
-		return this.repository.findAll();
-	}
-
-	public Profile createProfile(Profile profile) {
-		return this.repository.save(profile);
-	}
-
-	public boolean deleteProfile(Long id) {
-		this.repository.deleteById(id);
-		return !this.repository.existsById(id);
-	}
-
-	public Profile updateProfile(Profile profile, Long id) {
-		Optional<Profile> optProfile = this.repository.findById(id);
-		Profile oldProfile = optProfile.orElseThrow(() -> new ProfileNotFoundException());
-
-		oldProfile.setName(profile.getName());
-		oldProfile.setNationality(profile.getNationality());
-		oldProfile.setDescription(profile.getDescription());
-
-		Profile updatedProfile = this.repository.save(oldProfile);
-		return updatedProfile;
-
-	}
-}
+} 
